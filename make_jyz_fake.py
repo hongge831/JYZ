f44c68d22737e543480f53e0723c02aeddd3d09a
Fanfan Ye <hongge831@163.com>
2018-10-22 21:43:32 +0800

    add code

--- make_jyz_fake.py
+++ make_jyz_fake.py
@@ -4,12 +4,14 @@ import cv2
 from tqdm import tqdm
 from PIL import Image
 
+classone_src = ''
+dangeboli_src = ''
+classone_list = [os.path.join(classone_src,x) for x in classone_src]
+dangeboli_list = [os.path.join(dangeboli_src,x) for x in dangeboli_src]
+num = 10
 
-
-
-
-
-
+def isiou():
+    pass
 
 
 if __name__ == '__main__':
